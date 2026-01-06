9dbf4ad13ba3ca4a9e6601ac0f4c084c1c5ab352
sujitkumar-13 <sujitkumar19013@gmail.com>
2026-01-06 11:39:28 +0530

    feat: replace external gallery image URLs with new local assets and update GallerySection to use them.

--- frontend/src/components/GallerySection.tsx
+++ frontend/src/components/GallerySection.tsx
@@ -1,29 +1,30 @@
+import image1 from "../assets/Gallery/image1.avif"
+import image2 from "../assets/Gallery/image2.avif"
+import image3 from "../assets/Gallery/image3.avif"
+import image4 from "../assets/Gallery/image4.webp"
+import image5 from "../assets/Gallery/image5.avif"
+import image6 from "../assets/Gallery/image6.avif"
+import image7 from "../assets/Gallery/image7.jpg"
+import image8 from "../assets/Gallery/image8.jpg"
+import image9 from "../assets/Gallery/image9.avif"
+import image10 from "../assets/Gallery/image10.avif"
+import image11 from "../assets/Gallery/image11.webp"
+import image12 from "../assets/Gallery/image12.jpg"
+
 export const GallerySection = () => {
     const allImages = [
-        "https://content.jdmagicbox.com/comp/varanasi/k7/0542px542.x542.180227213015.h9k7/catalogue/bhawan-finance-varanasi-1gqsztqkpu.jpg?imwidth=463.3333333333333",
-
-        "https://content.jdmagicbox.com/comp/varanasi/k7/0542px542.x542.180227213015.h9k7/catalogue/bhawan-finance-varanasi-ho-varanasi-finance-companies-7g2r45dssb.jpg?imwidth=463.3333333333333",
-
-        "https://content.jdmagicbox.com/comp/varanasi/k7/0542px542.x542.180227213015.h9k7/catalogue/bhawan-finance-varanasi-6qata.jpg?imwidth=463.3333333333333",
-
-        "https://akm-img-a-in.tosshub.com/businesstoday/images/story/202509/68dbc2acbce78-built-in-1921-by-british-architect-herbert-baker--the-north-and-south-block-buildings-have-long-hous-30443958-16x9.jpg",
-
-        "https://content.jdmagicbox.com/comp/varanasi/k7/0542px542.x542.180227213015.h9k7/catalogue/bhawan-finance-varanasi-ho-varanasi-finance-companies-gasg8nkthc.jpg?imwidth=463.3333333333333",
-
-        "https://content.jdmagicbox.com/comp/varanasi/k7/0542px542.x542.180227213015.h9k7/catalogue/bhawan-finance-varanasi-ho-varanasi-finance-companies-c297htrqyx.jpg?imwidth=463.3333333333333",
-
-        "https://instagram.fdel27-7.fna.fbcdn.net/v/t39.30808-6/526187672_1565885634378078_4562754319540324628_n.jpg?stp=c0.146.791.988a_dst-jpg_e35_tt6&_nc_cat=105&ig_cache_key=MzY4ODY2MDU1NDYyMTgyNTk1Nw%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjc5MXgxMjgwLnNkci5DMyJ9&_nc_ohc=IqVaV9GxY_UQ7kNvwFGIZ5P&_nc_oc=AdknaTBBF0WXNTwQDFbbLEohg7u-1fSSueDvSzXHHsJ9Hbk2T9jCKzAWs_TE0zPM4EButfAx7d1aEnrmSjVrzPVE&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=instagram.fdel27-7.fna&_nc_gid=Kx3DvMuvQnLpSujIPJNCHA&oh=00_AfqWyg9F0zEzoww0XDDmsyauBr9pym4RYZtrT1nXZa4XjA&oe=696285FB",
-
-        "https://akm-img-a-in.tosshub.com/businesstoday/images/story/202509/68d39584a8e3e-finance-ministry-to-move-into-new-office-soon-245353831-16x9.jpg",
-
-        "https://content.jdmagicbox.com/comp/varanasi/k7/0542px542.x542.180227213015.h9k7/catalogue/bhawan-finance-varanasi-ho-varanasi-finance-companies-075ys3p016.jpg?imwidth=463.3333333333333",
-
-        "https://content3.jdmagicbox.com/comp/varanasi/k7/0542px542.x542.180227213015.h9k7/catalogue/bhawan-finance-varanasi-ho-varanasi-finance-companies-55ro3kmc6e.jpg",
-
-        "https://content.jdmagicbox.com/v2/comp/varanasi/k7/0542px542.x542.180227213015.h9k7/catalogue/bhawan-finance-varanasi-ho-varanasi-finance-companies-7tifysxejf.jpg?imwidth=463.3333333333333",
-
-
-        "https://instagram.fdel27-6.fna.fbcdn.net/v/t39.30808-6/599957785_1685101912456449_3326855554486891624_n.jpg?stp=dst-jpg_e35_p1080x1080_tt6&_nc_cat=111&ig_cache_key=Mzc4OTMxNTIzMTY5MzY1NDIzOQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE2Mzh4MjA0OC5zZHIuQzMifQ%3D%3D&_nc_ohc=PQJ5EPtT3zoQ7kNvwEnm7eL&_nc_oc=Adl6D6POdLV9cDudJE3w-B4xniFu_ipinnll38IjN2xMpIoZTdO3svY1sdCV5E_qFji5Tk9xKnZNzGtN11ZYrL33&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=instagram.fdel27-6.fna&_nc_gid=4cdaEB8xpuRiotGsnMw-Bw&oh=00_AfqFAiSzcCxzDGNBgVTd1h_xhmzYiPSZezcygonv4I3H4Q&oe=69628F93"
+        image1,
+        image2,
+        image3,
+        image4,
+        image5,
+        image6,
+        image7,
+        image8,
+        image9,
+        image10,
+        image11,
+        image12,
     ];
 
     return (
